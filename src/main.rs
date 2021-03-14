7993006f5ab2e921be668a2ebe024630cd0e1e46
daichi_yamaoka <tnychor06@gmail.com>
2021-03-14 23:16:29 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use clap::Clap;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0"
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(name = "FILE")]
+    formula_file: Option<String>
+}
+
+fn main() {
+    let opts = Opts::parse();
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+
+        run(reader, opts.verbose);
+    } else {
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose);
+    }
+}
+
+fn run<R: BufRead>(reader: R, _verbose: bool) {
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+    }
+}
